1cb6c2282887115caf15ac1d6e1c9c01fde7b620
Piyushh10 <shivnanip815@gmail.com>
2025-06-28 14:57:59 +0530

    now auto log out issue solved

--- app/src/main/java/com/example/codegram/ui/navigation/AccountNavigation.kt
+++ app/src/main/java/com/example/codegram/ui/navigation/AccountNavigation.kt
@@ -16,45 +16,73 @@ import com.example.codegram.ui.screens.LeetCodeStatsScreen
 import com.example.codegram.ui.screens.LeetCodeUsernameInputScreen
 import com.example.codegram.ui.screens.LoginScreen
 import com.example.codegram.viewmodel.LeetCodeStatsViewModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun AccountNavigation() {
     val navController = rememberNavController()
     val db = FirebaseDatabase.getInstance()
     val leetcodeViewModel = LeetCodeStatsViewModel()
+    val firebaseAuth = FirebaseAuth.getInstance()
+    var startDestination by remember { mutableStateOf<String?>(null) }
 
-    NavHost(navController = navController, startDestination = Screen.Login.route) {
-        composable(Screen.Login.route) { LoginScreen(navController) }
-        composable("LeetCodeUsernameInputScreen") { LeetCodeUsernameInputScreen(navController) }
-        composable(
-            route = Screen.LeetCodeStats.route,
-            arguments = listOf(navArgument("username") { type = NavType.StringType })
-        ) { navBackStackEntry ->
-            val username = navBackStackEntry.arguments?.getString("username")
-            if (username != null) {
-                LeetCodeStatsScreen(viewModel = leetcodeViewModel, username = username)
-            } else {
-                Log.e("LeetCodeStatsScreen", "Username is null")
+    LaunchedEffect(Unit) {
+        val user = firebaseAuth.currentUser
+        if (user == null) {
+            startDestination = Screen.Login.route
+        } else {
+            // Try to get the username from Firebase
+            val userId = user.uid
+            val dbRef = db.getReference("users").child(userId).child("leetCodeUsername")
+            dbRef.get().addOnSuccessListener { snapshot ->
+                val username = snapshot.getValue(String::class.java)
+                startDestination = if (!username.isNullOrBlank()) {
+                    Screen.LeetCodeStats.createRoute(username)
+                } else {
+                    "LeetCodeUsernameInputScreen"
+                }
+            }.addOnFailureListener {
+                startDestination = "LeetCodeUsernameInputScreen"
             }
         }
+    }
 
-        composable(Screen.Chat.route) {
-            ChatScreen(
-                chatHelper = ChatHelper(db, LocalContext.current),
-                navController = navController
-            )
-        }
-        composable(
-            route = Screen.GroupChat.route,
-            arguments = listOf(navArgument("groupName") { type = NavType.StringType })
-        ) { backStackEntry ->
-            val groupName = backStackEntry.arguments?.getString("groupName")
-            groupName?.let {
-                val chatHelper = ChatHelper(db, LocalContext.current)
-                val selectedGroup = groups.find { it.name == groupName }
-                if (selectedGroup != null) {
-                    GroupChatScreen(group = selectedGroup, chatHelper = chatHelper, navController = navController)
+    if (startDestination != null) {
+        NavHost(navController = navController, startDestination = startDestination!!) {
+            composable(Screen.Login.route) { LoginScreen(navController) }
+            composable("LeetCodeUsernameInputScreen") { LeetCodeUsernameInputScreen(navController) }
+            composable(
+                route = Screen.LeetCodeStats.route,
+                arguments = listOf(navArgument("username") { type = NavType.StringType })
+            ) { navBackStackEntry ->
+                val username = navBackStackEntry.arguments?.getString("username")
+                if (username != null) {
+                    LeetCodeStatsScreen(viewModel = leetcodeViewModel, username = username)
+                }
+            }
+            composable(Screen.Chat.route) {
+                ChatScreen(
+                    chatHelper = ChatHelper(db, LocalContext.current),
+                    navController = navController
+                )
+            }
+            composable(
+                route = Screen.GroupChat.route,
+                arguments = listOf(navArgument("groupName") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val groupName = backStackEntry.arguments?.getString("groupName")
+                groupName?.let {
+                    val chatHelper = ChatHelper(db, LocalContext.current)
+                    val selectedGroup = groups.find { it.name == groupName }
+                    if (selectedGroup != null) {
+                        GroupChatScreen(group = selectedGroup, chatHelper = chatHelper, navController = navController)
+                    }
                 }
             }
         }
